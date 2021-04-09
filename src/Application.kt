51125d328dc84e1ed224efa9ff42357ea82f1d96
mahmoudmabrok <mahmoudmabrok3579@gmail.com>
2021-04-09 23:20:24 +0200

    feat: - add PUT - add some validation to post/put endpoints

--- src/Application.kt
+++ src/Application.kt
@@ -1,11 +1,11 @@
 package com.example
 
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
 import io.ktor.serialization.*
 
 val students = mutableListOf<Student>().apply {
@@ -48,9 +48,6 @@ fun Application.module(testing: Boolean = false) {
                 // return that student
                 call.respond(student)
 
-
-
-
             }
 
             // add a student
@@ -58,19 +55,53 @@ fun Application.module(testing: Boolean = false) {
                 try {
                     // `receive` will get content from request and convert it to Student object
                     val student = call.receive<Student>()
-                    students.add(student)
-                    call.respondText("Added " , status = HttpStatusCode.Created)
+                    if (students.filter { it.id == student.id }.isEmpty()) {
+                        students.add(student)
+                        call.respondText("Added student ${student.name}", status = HttpStatusCode.Created)
+                    } else {
+                        call.respondText("Already exist student with same id", status = HttpStatusCode.BadRequest)
+                    }
                 } catch (e: Exception) {
-                    call.respondText("Error ${e.message}",status = HttpStatusCode.BadRequest)
+                    call.respondText("Error ${e.message}", status = HttpStatusCode.BadRequest)
+                }
+            }
+
+            put("{id}") {
+                try {
+                    val id = call.parameters["id"] ?: return@put call.respondText(
+                        "Missing Paramater [id]",
+                        status = HttpStatusCode.BadRequest
+                    )
+                    // `receive` will get content from request and convert it to Student object
+                    val student = call.receive<Student>()
+                    if (id != student.id) {
+                        call.respondText("Not matched student id $id", status = HttpStatusCode.BadRequest)
+                        return@put
+                    }
+                    val index = students.indexOfFirst { it.id == id }
+                    if (index != -1) {
+                        students[index] = student
+                        call.respondText("updated student $student", status = HttpStatusCode.OK)
+                    } else {
+                        call.respondText("not exist student with id ", status = HttpStatusCode.BadRequest)
+                    }
+                } catch (e: Exception) {
+                    call.respondText("Error ${e.message}", status = HttpStatusCode.BadRequest)
                 }
             }
 
             // delete a student with `id`
-            delete("{id}"){
-                val id = call.parameters["id"] ?: return@delete call.respondText("Missing Paramater [id]", status = HttpStatusCode.BadRequest)
-                val student = students.find { it.id == id } ?: return@delete call.respondText("No students with id $id",status = HttpStatusCode.NotFound )
+            delete("{id}") {
+                val id = call.parameters["id"] ?: return@delete call.respondText(
+                    "Missing Paramater [id]",
+                    status = HttpStatusCode.BadRequest
+                )
+                val student = students.find { it.id == id } ?: return@delete call.respondText(
+                    "No students with id $id",
+                    status = HttpStatusCode.NotFound
+                )
                 students.remove(student)
-                call.respondText("Deleted" , status = HttpStatusCode.Accepted)
+                call.respondText("Deleted", status = HttpStatusCode.Accepted)
             }
 
 
